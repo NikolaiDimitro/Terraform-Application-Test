812ecf6de34571ec1a4b58d955c527aff64fbd16
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2026-03-29 17:44:26 +0300

    Update

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -18,7 +22,7 @@ resource "random_integer" "ri" {
 }
 
 # Resource Group
-resource "azurerm_resource_group" "arg" {
+resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_group_name}-${random_integer.ri.result}"
   location = var.resourse_group_location
 }
@@ -26,26 +30,26 @@ resource "azurerm_resource_group" "arg" {
 # Service Plan
 resource "azurerm_service_plan" "asp" {
   name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
-  resource_group_name = azurerm_resource_group.arg.name
-  location            = azurerm_resource_group.arg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "F1"
 }
 
 # SQL Server
-resource "azurerm_mssql_server" "asqlserver" {
+resource "azurerm_mssql_server" "sql" {
   name                         = substr("${lower(var.sql_server_name)}-${random_integer.ri.result}", 0, 63)
-  resource_group_name          = azurerm_resource_group.arg.name
-  location                     = azurerm_resource_group.arg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = var.sql_admin_login
   administrator_login_password = var.sql_admin_password
 }
 
-# Firewall Rule
+# SQL Firewall Rule
 resource "azurerm_mssql_firewall_rule" "fw" {
-  name             = var.firewall_rule_name
-  server_id        = azurerm_mssql_server.asqlserver.id
+  name             = "AllowAllIPs"
+  server_id        = azurerm_mssql_server.sql.id
   start_ip_address = "0.0.0.0"
   end_ip_address   = "0.0.0.0"
 }
@@ -53,20 +57,19 @@ resource "azurerm_mssql_firewall_rule" "fw" {
 # Database
 resource "azurerm_mssql_database" "db" {
   name                 = var.sql_database_name
-  server_id            = azurerm_mssql_server.asqlserver.id
+  server_id            = azurerm_mssql_server.sql.id
   collation            = "SQL_Latin1_General_CP1_CI_AS"
   sku_name             = "S0"
   zone_redundant       = false
   storage_account_type = "Local"
 }
 
-# Web App
+# Linux Web App
 resource "azurerm_linux_web_app" "webapp" {
   name                = "${var.app_service_name}-${random_integer.ri.result}"
-  resource_group_name = azurerm_resource_group.arg.name
-  location            = azurerm_resource_group.arg.location
-
-  service_plan_id = azurerm_service_plan.asp.id
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
     application_stack {
@@ -78,13 +81,6 @@ resource "azurerm_linux_web_app" "webapp" {
   connection_string {
     name  = "DefaultConnection"
     type  = "SQLAzure"
-    value = "Data Source=tcp:${azurerm_mssql_server.asqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};User ID=${azurerm_mssql_server.asqlserver.administrator_login};Password=${azurerm_mssql_server.asqlserver.administrator_login_password};Trusted_Connection=False;MultipleActiveResultSets=True;"
+    value = "Server=tcp:${azurerm_mssql_server.sql.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.db.name};User ID=${azurerm_mssql_server.sql.administrator_login};Password=${azurerm_mssql_server.sql.administrator_login_password};MultipleActiveResultSets=True;"
   }
-}
-
-# GitHub deployment
-resource "azurerm_app_service_source_control" "github" {
-  app_id   = azurerm_linux_web_app.webapp.id
-  repo_url = var.repo_URL
-  branch   = "main"
 }
